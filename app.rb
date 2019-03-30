312707aa22b7629ffaf80ad6fdb7e30f825a7bb7
Giovanni Guzman <giomaster022@gmail.com>
2019-03-30 10:06:24 -0700

    agregado metodo calcular

--- app.rb
+++ app.rb
@@ -15,15 +15,55 @@ puts Inventory.manual_transmissions
 puts Inventory.automatic_transmissions
 
 # How to build a car object
-car = Car.new(
-  brand:        "Mercedes",
-  name:         "Citan",
+# car = Car.new(
+#   brand:        "Mercedes",
+#   name:         "Citan",
+#   tires:        "Continental",
+#   motor:        "Turbo 2.5",
+#   transmission: "Manual",
+#   doors:        2,
+#   style:        "Sedan"
+# )
+
+# Crear un carro Golf
+golf = Car.new(
+  brand:        "Volkswagen",
+  name:         "Golf",
   tires:        "Continental",
   motor:        "Turbo 2.5",
-  transmission: "Manual",
-  doors:        2,
+  transmision:  "Manual",
+  doors:        5,
+  style:        "Hashback"
+)
+
+# Crear un carro Beatle
+beetle = Car.new(
+  brand:        "Volkswagen",
+  name:         "Beetle",
+  tires:        "Michelin",
+  motor:        "Non Turbo 2.0",
+  transmission: "Automatic",
+  doors:        4,
   style:        "Sedan"
 )
+
+# Metodo que calcula la cantidad de carros que se pueden crear
+def calcular(carro)
+  if carro.name = "Golf"
+    tires = Inventory.continental_tires/carro.tires
+    motor = Inventory.motors_with_turbo
+    transmision = Inventory.manual_transmissions
+    doors = Inventory.doors/carro.doors
+  else
+    tires = Inventory.michelin_tires/carro.tires
+    motor = Inventory.motors_with_no_turbo
+    transmision = Inventory.automatic_transmissions
+    doors = Inventory.doors/carro.doors
+  end
+  cantidad = [tires, motor, transmision, doors].min
+  puts cantidad
+  return cantidad
+end
 
 # Metodo que crea un arreglo de multiples copias de un mismo carro
 def crearCarro(carro,cantidad)
